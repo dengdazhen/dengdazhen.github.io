0592ca370aa17afa780d24621451dd4fd2f9187a
Dazhen <dazhen@DazhendeMacBook-Pro.local>
2019-10-07 01:29:44 +0800

    Updates

---
+++ precache-manifest.6d9bfd4b16741891094f2a028e946adf.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31636b6ae1b17101f2d77f8c6ac72d5d",
+    "url": "/homePage/index.html"
+  },
+  {
+    "revision": "224e1037d27f99c32200",
+    "url": "/homePage/static/js/2.dd14dee2.chunk.js"
+  },
+  {
+    "revision": "054d13d67caa9b50c290",
+    "url": "/homePage/static/js/main.9504088a.chunk.js"
+  },
+  {
+    "revision": "5f0f97d8f49c892537ce",
+    "url": "/homePage/static/js/runtime-main.42f371d1.js"
+  }
+]);
