3e554599a21e8ba9f8841cdeb158bde3252fdbb4
dazhen <dengdazhen@outlook.com>
2024-07-30 13:37:31 +0800

    update

--- src/components/content.js
+++ src/components/content.js
@@ -37,15 +37,15 @@ const data = {
     news: [
         {
             date: "07/2024",
-            content: 'A paper was accepted to IEEE VIS 2024.'
+            content: 'A paper accepted to IEEE VIS 2024.'
         },
         {
             date: "04/2024",
-            content: 'A paper about "sports news generation" was accepted to TVCG.'
+            content: 'A paper about "sports news generation" accepted to TVCG.'
         },
         {
             date: "01/2024",
-            content: 'A paper about "chart generation" was accepted to TVCG.'
+            content: 'A paper about "chart generation" accepted to TVCG.'
         },
         {
             date: "12/2023",
